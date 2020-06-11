213f3e487e88f4cf64d71d5d43f9c7ba968d8296
Swarnali Chakraborty (swarchak) <swarchak@cisco.com>
2020-06-11 15:00:30 +0530

    Added user profile feeds api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,6 +8,8 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.USerProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
+
 
 
 urlpatterns = [
