09a8babe0d54fe1b7423a1d2a58e58a35ba49cab
Swarnali Chakraborty (swarchak) <swarchak@cisco.com>
2020-06-12 08:47:26 +0530

    Configured for deployment

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0gx3v&*!lh@akovmo*n2rvll79$b-=x9+bu(9p3#w4aw_ghn!='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environment.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
